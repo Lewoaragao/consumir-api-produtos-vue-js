714138255a81ed119d65da54769b579f65509886
Leonardo Aragão <lewoaragao@gmail.com>
2022-08-27 18:03:51 -0300

    config.js para producao

--- src/services/config.js
+++ src/services/config.js
@@ -2,5 +2,6 @@
 import axios from 'axios'
 
 export const http = axios.create({
-    baseURL: 'http://localhost:8080/api/'
+    // baseURL: 'http://localhost:8080/api/'
+    baseURL: 'https://api-rest-spring-produtos-lca.herokuapp.com/api/'
 })
